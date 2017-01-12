9425edc475077751889d25e879ef5cc1152edeb6
Alex Ruiz Estradera <ahharu@ahharu.com>
2017-01-12 16:43:31 +0100

    fix collector

--- manifests/analytics.pp
+++ manifests/analytics.pp
@@ -105,8 +105,8 @@ class midonet::analytics (
   if versioncmp($midonet_version,'5.2') > 0
   {
     $config = { 'network.host' => ['_local_',"${elk_bind_ip}"]}
-    Apt::Source<| title = 'logstash' |> {location => "http://packages.elastic.co/logstash/${logstash_version}/debian"}
-    Yumrepo::Logstash<| title = 'logstash' |> {baseurl => "http://packages.elastic.co/logstash/${logstash_version}/centos"}
+    Apt::Source       <| title == 'logstash' |> {location => "http://packages.elastic.co/logstash/${logstash_version}/debian"}
+    Yumrepo::Logstash <| title == 'logstash' |> {baseurl => "http://packages.elastic.co/logstash/${logstash_version}/centos"}
   }
   else {
     $config = undef
